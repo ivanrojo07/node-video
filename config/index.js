46e7bb6f3c77c7e29437920d5ec7f04f2f6f0594
Ivan Rojo <ivanrojo07@gmail.com>
2021-11-08 18:14:07 -0600

    conexión a db

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    cors: process.env.CORS || '*',
+    dbUser: process.env.DB_USER || 'user',
+    dbPass: process.env.DB_PASSWORD || '1234567890',
+    dbHost: process.env.DB_HOST || 'puerta.db.com',
+    dbName: process.env.DB_NAME || 'video'
 }
 
 module.exports = { config }
