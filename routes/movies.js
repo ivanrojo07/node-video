3cca1c52cc059b1e66db68b060acc0e4b22398dd
Ivan Rojo <ivanrojo07@gmail.com>
2021-11-09 18:50:24 -0600

    Cache-Control in list and get endpoint

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,10 @@ const express = require('express');
 const MoviesService = require('../services/movies');
 const { movieIdSchema, createMovieSchema, updateMovieSchema } = require('../utils/schemas/movie');
 const validationHandler = require('../utils/middleware/validationHandler');
+
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
+
 function moviesApi(app) {
     const router = express.Router();
 
@@ -10,6 +14,7 @@ function moviesApi(app) {
     const moviesService = new MoviesService();
 
     router.get('/', async function(req, res, next) {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { tags } = req.query;
         try {
             
@@ -25,6 +30,7 @@ function moviesApi(app) {
     });
 
     router.get('/:movieId',  validationHandler({ movieId: movieIdSchema }, 'params'), async function(req, res, next) {
+        cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
         const { movieId } = req.params;
         try {
             const movies = await moviesService.getMovie(movieId);
